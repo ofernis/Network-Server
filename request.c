0e3a2a7548be401786a5bbd73cdef937ea9fb9bf
ofernis <ofernis73@gmail.com>
2022-06-15 23:29:24 +0300

    waitpid

--- request.c
+++ request.c
@@ -128,14 +128,16 @@ void requestServeDynamic(int fd, char *filename, char *cgiargs, struct stats_t s
    sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n", buf, stats.handler_thread_stats.handler_thread_dynamic_req_count[id]);
    Rio_writen(fd, buf, strlen(buf));
    
-   if (Fork() == 0) {
+   int pid = Fork();
+   if (pid == 0) {
       /* Child process */
       Setenv("QUERY_STRING", cgiargs, 1);
       /* When the CGI process writes to stdout, it will instead go to the socket */
       Dup2(fd, STDOUT_FILENO);
       Execve(filename, emptylist, environ);
    }
-   Wait(NULL);
+
+   WaitPid(pid, NULL, 0);
 }
 
 
